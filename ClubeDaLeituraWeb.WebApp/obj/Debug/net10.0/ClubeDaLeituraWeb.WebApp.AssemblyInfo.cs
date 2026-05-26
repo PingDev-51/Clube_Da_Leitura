2960bfdc03d0a939c53278170b25008dcb76211d
Kauan Galvani <kauanrafaelgalvani@gmail.com>
2026-05-26 16:15:11 -0300

    adicionado arquivos Base

--- ClubeDaLeituraWeb.WebApp/obj/Debug/net10.0/ClubeDaLeituraWeb.WebApp.AssemblyInfo.cs
+++ ClubeDaLeituraWeb.WebApp/obj/Debug/net10.0/ClubeDaLeituraWeb.WebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClubeDaLeituraWeb.WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30042e380144d1f054ca996dd043e43a511d1f9e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClubeDaLeituraWeb.WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClubeDaLeituraWeb.WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
