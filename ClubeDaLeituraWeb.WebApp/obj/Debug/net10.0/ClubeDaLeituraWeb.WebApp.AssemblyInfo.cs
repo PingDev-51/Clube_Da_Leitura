2b2e4fed6ed8a742cb3cb2ee751897510287d654
Kauan Silva <kauanmacedo338@gmail.com>
2026-05-26 23:07:00 -0300

    acept

--- ClubeDaLeituraWeb.WebApp/obj/Debug/net10.0/ClubeDaLeituraWeb.WebApp.AssemblyInfo.cs
+++ ClubeDaLeituraWeb.WebApp/obj/Debug/net10.0/ClubeDaLeituraWeb.WebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClubeDaLeituraWeb.WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2960bfdc03d0a939c53278170b25008dcb76211d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5309fb5864f91b42eda0dae1501427bf8ca0b35")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClubeDaLeituraWeb.WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClubeDaLeituraWeb.WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
