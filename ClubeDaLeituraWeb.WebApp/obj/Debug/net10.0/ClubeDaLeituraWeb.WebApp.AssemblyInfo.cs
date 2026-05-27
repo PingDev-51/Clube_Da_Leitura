b0126795606849400af52b914cfd780652d532b7
Kauan Silva <kauanmacedo338@gmail.com>
2026-05-27 18:11:25 -0300

    adicionado modulo de revistas

--- ClubeDaLeituraWeb.WebApp/obj/Debug/net10.0/ClubeDaLeituraWeb.WebApp.AssemblyInfo.cs
+++ ClubeDaLeituraWeb.WebApp/obj/Debug/net10.0/ClubeDaLeituraWeb.WebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClubeDaLeituraWeb.WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd0797fe56d9dad11709a5e0876ec9505d950ba1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f06cc6cb972d8ee35e827ce7385098af0c2d24e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClubeDaLeituraWeb.WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClubeDaLeituraWeb.WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
