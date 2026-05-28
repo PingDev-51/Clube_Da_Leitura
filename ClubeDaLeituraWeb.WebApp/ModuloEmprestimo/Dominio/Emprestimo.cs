e117be9b042e3bf53289c418b236a15a836f51ba
Kauan Galvani <kauanrafaelgalvani@gmail.com>
2026-05-28 11:45:49 -0300

    Criado arquivos Emprestimo

---
+++ ClubeDaLeituraWeb.WebApp/ModuloEmprestimo/Dominio/Emprestimo.cs
@@ -0,0 +1,79 @@
+using System.Security.Cryptography;
+using ClubeDaLeituraWeb.WebApp.ModuloRevistas.Dominio;
+
+namespace ClubeDaLeitura.ConsoleApp.Dominio;
+
+public class Emprestimo
+{
+    public string Id { get; private set; } = string.Empty;
+    public Revista Revista { get; private set; }
+    public Amigo Amigo { get; private set; }
+    public StatusEmprestimo Status { get; private set; }
+    public DateTime DataAbertura { get; private set; }
+    public DateTime DataConclusaoPrevista
+    {
+        get
+        {
+            int diasDeEmprestimo = Revista.Caixa.DiasDeEmprestimo;
+
+            DateTime conclusao = DataAbertura.AddDays(diasDeEmprestimo);
+
+            return conclusao;
+        }
+    }
+
+    public bool EstaAtrasado
+    {
+        get
+        {
+            return Status == StatusEmprestimo.Aberto && DateTime.Now > DataConclusaoPrevista;
+        }
+    }
+
+    public Emprestimo(Revista revista, Amigo amigo)
+    {
+        Id = Convert
+                .ToHexString(RandomNumberGenerator.GetBytes(4))
+                .ToLower()
+                .Substring(0, 7);
+
+        Revista = revista;
+        Amigo = amigo;
+    }
+
+    public string[] Validar()
+    {
+        string erros = string.Empty;
+
+        if (Revista == null)
+            erros = "O campo \"Revista\" deve ser preenchido;";
+
+        else if (Revista.Status != StatusRevista.Disponivel)
+            erros = "A revista selecionada não está disponível.";
+
+        if (Amigo == null)
+            erros = "O campo \"Amigo\" deve ser preenchido;";
+
+        return erros.Split(';', StringSplitOptions.RemoveEmptyEntries);
+    }
+
+    public void Abrir()
+    {
+        DataAbertura = DateTime.Now;
+        Status = StatusEmprestimo.Aberto;
+
+        Revista.Emprestar();
+        Amigo.AdicionarEmprestimo(this);
+    }
+
+    public void Concluir()
+    {
+        Status = StatusEmprestimo.Concluido;
+        Revista.Devolver();
+    }
+
+    public int ObterQuantidadeDiasAtraso(DateTime dataConclusao)
+    {
+        return (dataConclusao - DataConclusaoPrevista).Days;
+    }
+}
--- ClubeDaLeituraWeb.WebApp/obj/Debug/net10.0/ClubeDaLeituraWeb.WebApp.AssemblyInfo.cs
+++ ClubeDaLeituraWeb.WebApp/obj/Debug/net10.0/ClubeDaLeituraWeb.WebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClubeDaLeituraWeb.WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0126795606849400af52b914cfd780652d532b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ef25ea67ded9294ee00226e4c15a8d5eb7ce693")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClubeDaLeituraWeb.WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClubeDaLeituraWeb.WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
